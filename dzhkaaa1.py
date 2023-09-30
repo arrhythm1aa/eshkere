e219507397c41ebd7e9f5f3e4de0a9f5d7bf35f6
arrhythm1aa <145200090+arrhythm1aa@users.noreply.github.com>
2023-09-30 14:21:45 +0300

    Add files via upload

---
+++ dzhkaaa1.py
@@ -0,0 +1,10 @@
+a = (int(input()))
+b = (int(input()))
+c = (int(input()))
+d = b ** 2 - 4 * a * c
+if d > 0:
+    print((-b + d ** 0.5)/(2 * a), (-b - d ** 0.5)/(2 * a))
+if d == 0:
+    print(-b / 2 * a)
+if d < 0:
+    print("Корней нет")
